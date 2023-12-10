6a797b82f1a6dc03560a7daadbb53aabf73b4937
Astaroth33 <francobahez333@gmail.com>
2023-12-10 17:40:56 -0600

    base

--- src/build/keys.js
+++ src/build/keys.js
@@ -5,7 +5,7 @@ exports.default = {
         host: 'juegosutm.cwzwtazyslex.us-east-2.rds.amazonaws.com',
         user: 'root',
         password: 'S4t4n33*',
-        database: 'juegosutm',
+        database: 'portal_videojuegos',
         port: 3306
     }
 };
